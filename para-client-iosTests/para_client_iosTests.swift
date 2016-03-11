525d7d12d9a7f7ae504726b802c091a5953fb78f
Alexander Bogdanovski <sashko5@gmail.com>
2016-03-11 16:01:20 +0200

    Initial Commit

---
+++ para-client-iosTests/para_client_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  para_client_iosTests.swift
+//  para-client-iosTests
+//
+//  Created by Alex on 11.03.16 г..
+//  Copyright © 2016 г. Erudika. All rights reserved.
+//
+
+import XCTest
+@testable import para_client_ios
+
+class para_client_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
